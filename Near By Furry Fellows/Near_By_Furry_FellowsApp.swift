dabc94e6a3d4258d41ffab9457f2b3a4ff89f719
jungsun-eoh <jungsun.eoh@gmail.com>
2021-12-01 13:35:42 -0800

    Initial Commit

---
+++ Near By Furry Fellows/Near_By_Furry_FellowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Near_By_Furry_FellowsApp.swift
+//  Near By Furry Fellows
+//
+//  Created by Jungsun Eoh on 12/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Near_By_Furry_FellowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
